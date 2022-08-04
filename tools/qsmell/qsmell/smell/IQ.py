c27023694f29a7185b85ed46460ebe8eff422778
jose <jcmc@fe.up.pt>
2022-08-04 01:52:10 +0100

    Re-implemented IQ.

--- tools/qsmell/qsmell/smell/IQ.py
+++ tools/qsmell/qsmell/smell/IQ.py
@@ -1,5 +1,6 @@
 """ Initialization of qubits differently from |0⟩ (IQ) """
 
+import sys
 import pandas as pd
 from .ISmell import *
 
@@ -9,21 +10,39 @@ class IQ(ISmell):
         super().__init__("IQ")
 
     def compute_metric(self, df: pd.DataFrame, output_file_path: str) -> None:
-        result_list = list()
-        for row in content:
-            row = row[0]
-            row_list = row.split(';')
-            row_list = list(filter(lambda x: (x.strip() != ""), row_list))
-            tf_table = row_list[1:]
-            index_list = list()
-            for index, val in enumerate(tf_table):
-                if val == '1':
-                    index_list.append(index)
-                    if len(index_list) == 2:
-                        result_list.append(index_list)
-                        break
-        new_result_list = list()
-        for inner_list in result_list:
-            diff = inner_list[1] - inner_list[0]
-            new_result_list.append(diff)
-        return new_result_list
+        qubits = [bit for bit in df.index if bit.startswith('q-')]
+        stamps = df.columns
+
+        metrics = {
+            'qubit': [],
+            'max_num_ops_between_init_and_use': []
+        }
+
+        for qubit in qubits:
+            max_num_ops_between_init_and_use = 0
+            count = -1
+            for stamp in stamps:
+                op = df.loc[qubit][stamp]
+                if op.lower().startswith('barrier'):
+                    continue
+
+                if op == '' and count == -1:
+                    # The first operation on qubit has not been found yet
+                    pass
+                elif op != '' and count == -1:
+                    # Found the first operation on qubit
+                    count = 0
+                elif op != '' and count != -1:
+                    # Found the second operation on qubit
+                    max_num_ops_between_init_and_use = max(max_num_ops_between_init_and_use, count)
+                    break
+                elif op == '' and count != -1:
+                    # Empty operation in between
+                    count += 1
+
+            metrics['qubit'].append(qubit)
+            metrics['max_num_ops_between_init_and_use'].append(max_num_ops_between_init_and_use)
+
+        out_df = pd.DataFrame.from_dict(metrics)
+        sys.stdout.write(str(out_df) + '\n')
+        out_df.to_csv(output_file_path, header=True, index=False, mode='w')
